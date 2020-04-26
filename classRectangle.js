6c56c0afe207d7c27589aaeb2eab7406172be3aa
Andranik Tsaturyan <andtsatur1296@gmail.com>
2020-04-26 18:53:04 +0400

    clas

---
+++ classRectangle.js
@@ -0,0 +1,21 @@
+class Rectangle{
+	constructor(height, width){
+		this.height = height;
+		this.width = width;
+	}
+	getArea(){
+		return this.height * this.width;
+	}
+	getPerimeter(){
+		return this.height * 2+this.width * 2; 
+	}
+	getDioganal(){
+		return Math.sqrt(this.height**2 + this.width**2);
+	}
+}
+let RectangleCopy = new Rectangle(10,10);
+console.log(RectangleCopy.getArea());
+let RectangleCopy1 = new Rectangle(15,15);
+console.log(RectangleCopy1.getPerimeter());
+let Rec = new Rectangle(3,4);
+console.log(Rec.getDioganal());
